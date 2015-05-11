da95cd6ee615eebdf9dd265f5c91090bf7e1e3dd
Chad Huneycutt <chadh@cc.gatech.edu>
2015-05-11 13:23:00 -0400

    creates initial checkin

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# roles #
+
+This is the roles module. It provides a set of roles that can be assigned to
+nodes (one role per node).  Roles are composed of profiles.
---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# Class: roles
+#
+# This module manages roles
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+class roles {
+
+}
